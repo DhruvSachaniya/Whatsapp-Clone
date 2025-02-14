b592743e61e5af40c5b54327e636daa0972fca08
Dhruv <sachaniyadhruv@email.com>
2025-02-14 20:52:53 +0530

    fix:- Chat Bug 14-02-2025

--- src/Chat/entities/chat.entity.ts
+++ src/Chat/entities/chat.entity.ts
@@ -1,15 +1,15 @@
 import { User } from 'src/auth/entities/user.entity';
 import { AbstractEntity } from 'src/database/abstract.entity';
-import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 
 @Entity()
 export class Chat extends AbstractEntity<Chat> {
     // user_1 connect with userid
-    @OneToOne(() => User, (user) => user.id)
+    @ManyToOne(() => User, (user) => user.id)
     @JoinColumn()
     user_1: User;
     // user_2
-    @OneToOne(() => User, (user) => user.id)
+    @ManyToOne(() => User, (user) => user.id)
     @JoinColumn()
     user_2: User;
     // meassages list insted of this add chat id as connnection
